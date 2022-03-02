565c5fdbf4774eaadc0483210f738e7a4c736814
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2022-03-02 12:00:02 +0100

    fix: missing video id

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 const run = async (req, res) => {
   const { video_id: videoId, url: videoUrl, proxy = false } = req.query;
-  if (!videoId || !videoUrl) throw Error("[MISSING] VIDEO_ID OR URL");
+  if (!videoId && !videoUrl) throw Error("[MISSING] VIDEO_ID OR URL");
 
   const url = await ytdl
     .getInfo(videoId ? `https://www.youtube.com/watch?v=${videoId}` : videoUrl)
